7b937f31aacb813551e6828d113edba45adbad41
User <User@Users-MacBook-Pro.local>
2019-09-30 14:23:38 +0600

    Сделал этап1. Перепсал все страницы в блоге с использованием классовых представлений View или TemplateView

---
+++ source/webapp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from webapp.models import Article, Comment, Category
+
+admin.site.register(Article)
+
+admin.site.register(Comment)
+
+admin.site.register(Category)
+
+
+
+
+# Register your models here.
